dfa865998d78b002997ada41a6d1189ea6862ffd
Luke Murphy <lukewm@riseup.net>
2019-12-04 22:51:11 +0700

    Appease the linter

--- test/test_version.py
+++ test/test_version.py
@@ -4,5 +4,7 @@
 def test_version_fails_gracefully(mocker):
     target = 'pkg_resources.get_distribution'
     mocker.patch(target, side_effect=Exception())
+
     from flat_tree.__init__ import __version__
+
     assert __version__ == 'unknown'
